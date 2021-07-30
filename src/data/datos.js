b1095924aa9691c67d7ad7bd88175638ba6904ed
ChristhianSM <christhian2524@gmail.com>
2021-07-30 13:04:58 -0500

    Agrega modal para mostrar producto

--- src/data/datos.js
+++ src/data/datos.js
@@ -1,61 +1,99 @@
-export const productosDestacados = [
-    {
-        id: 1,
-        nombre: "Producto1",
-        precio : 1500,
-        img : "../src/img/img1.jpg"
-    },
-    {
-        id: 2,
-        nombre: "Producto2",
-        precio : 1500,
-        img : "../src/img/img2.jpg"
-    },
-    {
-        id: 3,
-        nombre: "Producto3",
-        precio : 1500,
-        img : "../src/img/img4.jpg"
-    },
-
-]   
-
 export const productos = [
     {
         id: 1,
-        nombre: "Producto1",
+        nombre: "Bombé Max",
         precio : 1500,
-        img : "../src/img/img1.jpg"
+        img : "../src/img/img1.jpg",
+        destacado : true, 
+        aromas : [
+            "Coco Vainilla",
+           " Gardenia",
+            "Verbena",
+            "Naranja pimienta",
+            "Nardo",
+            "Tilo"
+        ],
+        descripcion : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias animi, cumque aperiam, fuga incidunt dolores ullam quos veritatis voluptatibus sit similique hic in officiis corrupti modi consectetur dolore non repellendus."
     },
     {
         id: 2,
-        nombre: "Producto2",
+        nombre: "Soya Simple",
         precio : 1500,
-        img : "../src/img/img2.jpg"
+        img : "../src/img/img3.jpg",
+        destacado : true,
+        aromas : [
+            "Coco Vainilla",
+           " Gardenia",
+            "Verbena",
+            "Naranja pimienta",
+            "Nardo",
+            "Tilo"
+        ],
+        descripcion : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias animi, cumque aperiam, fuga incidunt dolores ullam quos veritatis voluptatibus sit similique hic in officiis corrupti modi consectetur dolore non repellendus."
     },
     {
         id: 3,
-        nombre: "Producto3",
+        nombre: "Cónica",
         precio : 1500,
-        img : "../src/img/img4.jpg"
+        img : "../src/img/img2.jpg",
+        destacado : true, 
+        aromas : [
+            "Coco Vainilla",
+           " Gardenia",
+            "Verbena",
+            "Naranja pimienta",
+            "Nardo",
+            "Tilo"
+        ],
+        descripcion : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias animi, cumque aperiam, fuga incidunt dolores ullam quos veritatis voluptatibus sit similique hic in officiis corrupti modi consectetur dolore non repellendus."
     },
     {
         id: 4,
-        nombre: "Producto3",
+        nombre: "Bombé Mini",
         precio : 1500,
-        img : "../src/img/img4.jpg"
+        img : "../src/img/img4.jpg",
+        destacado : false, 
+        aromas : [
+            "Coco Vainilla",
+           " Gardenia",
+            "Verbena",
+            "Naranja pimienta",
+            "Nardo",
+            "Tilo"
+        ],
+        descripcion : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias animi, cumque aperiam, fuga incidunt dolores ullam quos veritatis voluptatibus sit similique hic in officiis corrupti modi consectetur dolore non repellendus."
     },
     {
         id: 5,
-        nombre: "Producto1",
+        nombre: "Línea Metal",
         precio : 1500,
-        img : "../src/img/img1.jpg"
+        img : "../src/img/img5.jpg",
+        destacado : false, 
+        aromas : [
+            "Coco Vainilla",
+           " Gardenia",
+            "Verbena",
+            "Naranja pimienta",
+            "Nardo",
+            "Tilo"
+        ],
+        descripcion : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias animi, cumque aperiam, fuga incidunt dolores ullam quos veritatis voluptatibus sit similique hic in officiis corrupti modi consectetur dolore non repellendus."
     },
     {
         id: 6,
-        nombre: "Producto2",
+        nombre: "Soya + crochet",
         precio : 1500,
-        img : "../src/img/img2.jpg"
+        img : "../src/img/img6.jpg",
+        destacado : false,
+        aromas : [
+            "Coco Vainilla",
+           " Gardenia",
+            "Verbena",
+            "Naranja pimienta",
+            "Nardo",
+            "Tilo"
+        ], 
+        descripcion : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias animi, cumque aperiam, fuga incidunt dolores ullam quos veritatis voluptatibus sit similique hic in officiis corrupti modi consectetur dolore non repellendus."
     },
 
 ]   
--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-import {productosDestacados,productos} from '../data/datos.js'
+import {productos} from '../data/datos.js'
 
 /* Variables */
 const contenedorCardDestacados = document.querySelector('.contenedorDestacados');
@@ -10,27 +10,31 @@ document.addEventListener('DOMContentLoaded',() => {
 })
 
 function mostrarDestacados(){
-    productosDestacados.forEach(producto => {
-        const card = document.createElement('div');
-        card.classList.add('card');
-
-        const imagen = document.createElement('img');
-        imagen.src = producto.img;
-
-        const tituloProducto = document.createElement('p');
-        tituloProducto.classList.add('tituloProducto');
-        tituloProducto.textContent = producto.nombre;
-
-        const precioProducto = document.createElement('p');
-        precioProducto.classList.add('precioProducto');
-        precioProducto.textContent = `$${producto.precio}`;
-
-        card.appendChild(imagen)
-        card.appendChild(tituloProducto)
-        card.appendChild(precioProducto);
-        console.log(card);
-
-        contenedorCardDestacados.appendChild(card);
+    productos.forEach(producto => {
+        if (producto.destacado) {
+            const card = document.createElement('div');
+            card.classList.add('card');
+            card.dataset.id = producto.id;
+            card.dataset.bsToggle = "modal";
+            card.dataset.bsTarget = "#productoSeleccionado";
+    
+            const imagen = document.createElement('img');
+            imagen.src = producto.img;
+    
+            const tituloProducto = document.createElement('p');
+            tituloProducto.classList.add('tituloProducto');
+            tituloProducto.textContent = producto.nombre;
+    
+            const precioProducto = document.createElement('p');
+            precioProducto.classList.add('precioProducto');
+            precioProducto.textContent = `$${producto.precio}`;
+    
+            card.appendChild(imagen)
+            card.appendChild(tituloProducto)
+            card.appendChild(precioProducto);
+    
+            contenedorCardDestacados.appendChild(card);
+        }
     })
 }
 
@@ -38,6 +42,9 @@ function mostrarProductos() {
     productos.forEach(producto => {
         const card = document.createElement('div');
         card.classList.add('card');
+        card.dataset.id = producto.id
+        card.dataset.bsToggle = "modal";
+        card.dataset.bsTarget = "#productoSeleccionado";
 
         const imagen = document.createElement('img');
         imagen.src = producto.img;
@@ -46,15 +53,75 @@ function mostrarProductos() {
         tituloProducto.classList.add('tituloProducto');
         tituloProducto.textContent = producto.nombre;
 
-        // const precioProducto = document.createElement('p');
-        // precioProducto.classList.add('precioProducto');
-        // precioProducto.textContent = `$${producto.precio}`;
-
         card.appendChild(imagen)
         card.appendChild(tituloProducto)
-        // card.appendChild(precioProducto);
-        console.log(card);
 
         contenedorCardProductos.appendChild(card);
+    });
+
+    productoSeleccionado();
+}
+
+function productoSeleccionado(){
+    const cardsProducto = document.querySelectorAll('.card');
+    cardsProducto.forEach( producto => {
+        producto.addEventListener('click', () => {
+            const idProducto = parseInt(producto.dataset.id);
+            const productoEcontrado = productos.find(elemento => elemento.id === idProducto);
+
+            /* Mostramos la informacion en el modal */
+            const modalTitle = document.querySelector('.modal-title');
+            modalTitle.textContent = productoEcontrado.nombre;
+
+            const modalBody = document.querySelector('.modal-body');
+            modalBody.innerHTML = `
+                <div class = "info">
+                    <h3>Vela</h3>
+                    <h2>${productoEcontrado.nombre}</h2>
+                    <div class = "estrellas">
+                        <img src = './src/img/Estrella-06.png'/>
+                        <img src = './src/img/Estrella-06.png'/>
+                        <img src = './src/img/Estrella-06.png'/>
+                        <img src = './src/img/Estrella-06.png'/>
+                        <img src = './src/img/Estrella-06.png'/>
+                        <img src = './src/img/Estrella-06.png'/>
+                    </div>
+                    <p>${productoEcontrado.descripcion}</p>
+                    <div class = "aromas">
+                        <p>Aromas disponibles</p>
+                        <ul class = "listaAromas">
+                            ${productoEcontrado.aromas.map( aroma =>`<li>${aroma}</li>`).join('')}
+                        </ul>
+                    </div>
+                </div>
+                <div class = "imagenVela">
+                    <img src = ${productoEcontrado.img} />
+                </div>
+            `
+            const modalFooter = document.querySelector('.modal-Footer');
+            modalFooter.innerHTML = `
+                <div class="informacionPago">
+                    <p><span>Medios de pago : </span> Efectivo o transferencia Bancaria</p>
+                    <p><span>Envios : </span> Pick up gratuito en lobos - Pcia de Bs As - CABA Zona alta palermo</p>
+                  </div>
+                <a href="#" class="btnComprar" data-id = ${productoEcontrado.id}>Comprar</a>
+            `
+
+            comprarProducto();
+        })
+    })
+}
+
+function comprarProducto(){
+    const btnComprar = document.querySelector('.btnComprar');
+    btnComprar.addEventListener('click', ()=>{
+        /* Extraemos el id */
+        const idProducto = parseInt(btnComprar.dataset.id);
+        console.log(idProducto);
+
+        const productoEcontrado = productos.find(elemento => elemento.id === idProducto);
+        const {descripcion, nombre, precio} = productoEcontrado;
+        const URL = `https://api.whatsapp.com/send?phone=+051959686193&text=*_Soya_*%0A*Hola ¿Qué tal como estas?*%0A%0A*Quisiera confirmar la siguiente Orden:*%0A*${nombre}   ->Precio: $${precio}`
+        window.open(URL, "_blank")
     })
 }
