319b0d3ce6089df6effadef06c9941b1d8c6318a
ChristhianSM <christhian2524@gmail.com>
2021-09-02 20:04:24 -0500

    se agrego imagenes

--- src/data/datos.js
+++ src/data/datos.js
@@ -3,7 +3,7 @@ export const productos = [
         id: 1,
         nombre: "Bombé Max",
         precio : 1500,
-        img : ["../src/img/img1.jpg", "../src/img/bombe-mini-1.jpg", "../src/img/img3.jpg"],
+        img : ["../src/img/img1.jpg", "../src/img/bombe-max-2.jpg", "../src/img/bombe-max-3.jpg"],
         destacado : true, 
         aromas : [
             "Coco Vainilla",
@@ -18,9 +18,9 @@ export const productos = [
     },
     {
         id: 2,
-        nombre: "Soya Simple",
+        nombre: "Bubbles",
         precio : 1500,
-        img : ["../src/img/img3.jpg", "../src/img/soya-simple-1.jpg", "../src/img/soya-simple-2.jpg"],
+        img : ["../src/img/bubbles-3.jpg", "../src/img/bubbles-2.jpg","../src/img/bubbles-1.jpg"],
         destacado : true,
         aromas : [
             "Coco Vainilla",
@@ -29,8 +29,8 @@ export const productos = [
             "Naranja pimienta",
             "Nardo",
             "Tilo"
-        ],
-        descripcion : "Esta Soya es ideal para espacios como habitaciones o baños ya que combina con todos los espacios por su óptimo tamaño. Tiene una capacidad de 170 gr. ",
+        ], 
+        descripcion : "Es tendencia en el mundo y está en Soya! Son hermosas para decorar y aromatizar cualquier espacio. Acordate que si queres encenderla debes ponerla sobre alguna superficie.",
         tipo : "vela"
     },
     {
@@ -54,7 +54,7 @@ export const productos = [
         id: 4,
         nombre: "Bombé Mini",
         precio : 1500,
-        img : ["../src/img/img4.jpg", "../src/img/bombe-mini-1.jpg","../src/img/img4.jpg"],
+        img : ["../src/img/img4.jpg", "../src/img/bombe-mini-1.jpg","../src/img/bombe-mini-2.jpg"],
         destacado : false, 
         aromas : [
             "Coco Vainilla",
@@ -86,9 +86,9 @@ export const productos = [
     },
     {
         id: 6,
-        nombre: "Soya + crochet",
+        nombre: "Apagavelas",
         precio : 1500,
-        img : ["../src/img/img6.jpg", "../src/img/apagavela-1.jpg","../src/img/img6.jpg"],
+        img : ["../src/img/img6.jpg", "../src/img/apagavela-3.jpg","../src/img/apagavela-4.jpg"], //falta
         destacado : false,
         aromas : [
             "Coco Vainilla",
@@ -98,8 +98,59 @@ export const productos = [
             "Nardo",
             "Tilo"
         ], 
-        descripcion : "Esta edición especial es la más linda del mundo! le da calidez a cualquier espacio. Ideal para mesa de luz. El crochet es también artesanal.",
+        descripcion : "Al momento que quieras apagar tu Soya y la cera de ésta haya llegado hasta los extremos, te recomiendo que utilices un apagavelas para completar este proceso. Los Apagavelas de Soya son elaborados de manera artesanal, es decir que cada uno es único y no se repite. Son ideales para sumar a la decoración y para complementar un regalo.",
         tipo : "otro"
+    },
+    {
+        id: 7,
+        nombre: "Bombones",
+        precio : 1500,
+        img : ["../src/img/bombones-1.jpg", "../src/img/bombones-2.jpg","../src/img/bombones-3.jpg"],
+        destacado : false,
+        aromas : [
+            "Coco Vainilla",
+           " Gardenia",
+            "Verbena",
+            "Naranja pimienta",
+            "Nardo",
+            "Tilo"
+        ], 
+        descripcion : "Los bombones aromáticos para Hornitos, son una alternativa novedosa y súper práctica a la hora de perfumar tus ambientes.Perduran más que el aceite ya que no necesitan agua, No ahuma, no hierve, no salpica.Una vez que el bomboncito no tenga más aroma, podes retirarlo y colocar uno nuevo.",
+        tipo : "otro"
+    },
+    {
+        id: 8,
+        nombre: "Soya Simple",
+        precio : 1500,
+        img : ["../src/img/img3.jpg", "../src/img/soya-simple-3.jpg", "../src/img/soya-simple-4.jpg"],
+        destacado : false,
+        aromas : [
+            "Coco Vainilla",
+           " Gardenia",
+            "Verbena",
+            "Naranja pimienta",
+            "Nardo",
+            "Tilo"
+        ],
+        descripcion : "Esta Soya es ideal para espacios como habitaciones o baños ya que combina con todos los espacios por su óptimo tamaño. Tiene una capacidad de 170 gr. ",
+        tipo : "vela"
+    },
+    {
+        id: 9,
+        nombre: "Soya + Crochet",
+        precio : 1500,
+        img : ["../src/img/crochet-1.jpg", "../src/img/crochet-2.jpg","../src/img/crochet-3.jpg"],
+        destacado : false,
+        aromas : [
+            "Coco Vainilla",
+           " Gardenia",
+            "Verbena",
+            "Naranja pimienta",
+            "Nardo",
+            "Tilo"
+        ], 
+        descripcion : "Esta edición especial es la más linda del mundo! le da calidez a cualquier espacio. Ideal para mesa de luz. El crochet es también artesanal. ",
+        tipo : "vela"
     },
 
 ]   
